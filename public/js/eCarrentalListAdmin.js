c05a38a31ec3ee94f069e78e5c649206cb6f48dc
wondyfraw <wondyfraw@users.noreply.github.com>
2020-05-13 08:23:26 +0200

    Car copy registration client code

--- public/js/eCarrentalListAdmin.js
+++ public/js/eCarrentalListAdmin.js
@@ -167,43 +167,45 @@ function saveNewCar(){
 }
 saveNewCar();
 
-function addNewCarCopy(){
-   const carCopyForm = document.getElementById("carPocyForm");
-   carCopyForm.addEventListener("submit",function(event){
-          event.preventDefault();
-
-    const quantity = document.getElementById("inputuantity").value;
-    const plateNumber = document.getElementById("inputPlateNumber").value;
-    const price = document.getElementById("inputPrice").value;
-    const carId = document.getElementById("carID");
-    console.log(carId);
-    const carCopyId=0
-
-    const newCarCopyData={
-        "quantity":quantity,
-        "available":true,
-        "plateNumber":plateNumber,
-        "carColor":color,
-        "rentPrice" : price,
-        "carId": carId};
-        fetch("http://localhost:8080/careRent/carCopy/new",{
-            method:"post",
-            body:JSON.stringify(newCarCopyData),
-            headers:{
-                "Content-Type":"application/json"
-            } 
-        }).then(function(response){
-            return{"status":"ok"};
-        }) .then(function(jsonResponseData){
-
-        }).catch(function(error){
-            console.error(error);
-        }) 
-
-   });
-}
+//function addNewCarCopy(){
+    const carCopyForm = document.getElementById("carPocyForm");
+    console.log("Call Copy");
+    carCopyForm.addEventListener("submit",function(event){
+           event.preventDefault();
+ 
+     const quantity = document.getElementById("inputuantity").value;
+     const plateNumber = document.getElementById("inputPlateNumber").value;
+     const price = document.getElementById("inputPrice").value;
+     const carId = document.getElementById("carID").value;
+     console.log(carId);
+     const carCopyId=0
+ 
+     const newCarCopyData={
+         "quantity":quantity,
+         "available":true,
+         "plateNumber":plateNumber,
+         "rentPrice" : price,
+         "carId": carId};
+         fetch("http://localhost:8080/careRent/carCopy/new",{
+             method:"post",
+             body:JSON.stringify(newCarCopyData),
+             headers:{
+                 "Content-Type":"application/json"
+             } 
+         }).then(function(response){
+             return{"status":"ok"};
+         }) .then(function(jsonResponseData){
+ 
+         }).catch(function(error){
+             console.error(error);
+         }) 
+ 
+    });
+ //}
    
 })();
+
+
 
 
 
